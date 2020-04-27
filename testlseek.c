1a681fb26feddcedaa8725634db134b7a4d73d0e
Estefania <elopez17@hawk.iit.edu>
2020-04-26 21:13:13 -0500

    extent file testing

--- testlseek.c
+++ testlseek.c
@@ -5,15 +5,15 @@
 int main(int argc, char *argv[]){
     
     int fd;
-    
     fd = open("text.txt", O_CREATE | O_RDWR );
-    
-
-    write(fd, "This is for testing purposes. hello! test test test test\n" , 60);
+    write(fd, "This is for test purposes. WE write to this file before we call lseek()\n" , 80);
 
     if (lseek(fd, 8) < 0){
         printf(1, "Error changing pointer\n");
     }
+
+    write(fd, "\n", 1);
+
     // invalid offset
     int boff = lseek(fd, -1);
     if (boff < 0){
@@ -26,15 +26,15 @@ int main(int argc, char *argv[]){
         printf(1, "lseek() failed: invalid file desriptor\n");
     }
 
-    write(fd, "UPDATE\n", 7);
+   // write(fd, "\n", 1);
     close(fd);
 
     int fd2; 
 	fd2 = open("lseek.txt", O_CREATE | O_RDWR | O_EXTENT);
     char string[] = "Testing: Checking lseek functionality with EXTENT files ";
     write(fd2, string, sizeof(string));
-	lseek(fd2, 4);
-	char string2[] = "NEW\n";
+	lseek(fd2, 8);
+	char string2[] = "\n";
 	write(fd2, string2, sizeof(string2));
 	close(fd2);
 
